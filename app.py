96f7e53137406584ef12a33892d5f8aaa0739663
William <william.n.vanbuskirk@gmail.com>
2022-11-12 22:05:04 -0600

    update template

--- app.py
+++ app.py
@@ -8,26 +8,22 @@ import pandas as pd
 #from whitenoise import WhiteNoise
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__)
 server = app.server
 
 #server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_json("https://data.usaid.gov/resource/a3rc-nmf6.json")
 
 fig_country_value = px.histogram(df, x="country", y=["line_item_value"],
         barmode="stack", title='2020 Line Item Costs by Country ($)',
-        text_auto='.1s')
+        text_auto='.1s', template='plotly_dark')
 fig_country_value.update_layout(xaxis={'categoryorder':'total descending'})
 fig_country_value.update_layout(showlegend=False)
 
 fig_molecule_type = px.histogram(df, x="molecule_test_type", y=["line_item_value"],
         barmode="stack", title='2020 Line Item Costs by Molecule Test Type ($)',
-        text_auto='.1s')
+        text_auto='.1s', template='plotly_dark')
 fig_molecule_type.update_layout(xaxis={'categoryorder':'total descending'})
 fig_molecule_type.update_layout(showlegend=False)
 
